46fee0bebf1185dd9a1fbc2e80396b79dee45fc8
HiromiOkukawa <lipton.31---hms@ezweb.ne.jp>
2017-05-22 10:16:48 +0900

    マイグレーションとルーティング設定終了

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +14,28 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks', [
+        'tasks' => Task::orderBy('created_at', 'asc')->get()
+    ]);
+});
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+        ->withInput()
+        ->withErrors($validator);
+    }
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
+});
+
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+    return redirect('/');
 });
